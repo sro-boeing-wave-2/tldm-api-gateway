0050bd8070c298a2e718cce616eb194d679fe681
Anandkumar <anandkumar316sw@gmail.com>
2018-09-28 16:20:46 +0530

    changed ocelot middlewares order

--- api-gateway/Startup.cs
+++ api-gateway/Startup.cs
@@ -52,9 +52,9 @@ namespace api_gateway
             }
 
             app.UseCors("AppPolicy");
-            app.UseOcelot().Wait();
             app.UseWebSockets();
             app.UseMvc();
+            app.UseOcelot().Wait();
         }
     }
 }
